b7060ad6a176bff0df5f0383b4dec49b5795d3dd
Dombis <dombis.gg@gmail.com>
2022-04-25 23:07:53 +0300

    release button click

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Selenium
 {
@@ -35,6 +26,8 @@ namespace Selenium
             var dateFrom = (DateTime)DateFrome.SelectedDate;
             var dateTo = (DateTime)DateTo.SelectedDate;
             var value = Value.Text;
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"https://www.google.com/maps/search/%D1%80%D0%B5%D1%81%D1%82%D0%BE%D1%80%D0%B0%D0%BD+%D0%B3%D1%80%D1%83%D0%B7%D0%B8%D1%8F/@47.2232698,39.697817,12z/data=!3m1!4b1";
         }
     }
 }
