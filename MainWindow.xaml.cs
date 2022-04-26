04f0f070e5f5cba4383a8834632dd6f55bb883c4
Dombis <dombis.gg@gmail.com>
2022-04-26 14:43:55 +0300

    Implemented search from link

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,16 +18,13 @@ namespace Selenium
 
         private void StartBrn_Click(object sender, RoutedEventArgs e)
         {
-            if (DateFrome.SelectedDate == null || DateTo.SelectedDate == null || string.IsNullOrEmpty(Value.Text) )
+            if (string.IsNullOrEmpty(Value.Text))
             {
-                MessageBox.Show("Please set all cells and try again.");
+                MessageBox.Show("Будь уважніше, всавив якусь херню! Спробуй спочатку");
                 return;
             }
-            var dateFrom = (DateTime)DateFrome.SelectedDate;
-            var dateTo = (DateTime)DateTo.SelectedDate;
             var value = Value.Text;
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = @"https://www.google.com/maps/search/%D1%80%D0%B5%D1%81%D1%82%D0%BE%D1%80%D0%B0%D0%BD+%D0%B3%D1%80%D1%83%D0%B7%D0%B8%D1%8F/@47.2232698,39.697817,12z/data=!3m1!4b1";
+            IWebDriver driver = new ChromeDriver {Url = @value};
         }
     }
 }
