69f53da4b2c09f73f7bab8e1b4f1067d1f45ecb4
Dombis <dombis.gg@gmail.com>
2022-05-17 15:10:36 +0300

    trying create Excel table

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,7 +19,7 @@ namespace Selenium
         {
             bool avalible = false;
             int scrollingDelay = 0, value = 2000, value1 = 1500, value2 = 1000;
-
+            // checking true user input
             if (string.IsNullOrEmpty(Value.Text))
             {
                 MessageBox.Show("Відсутнє посилання для пошуку! Спробуй спочатку");
@@ -49,7 +51,7 @@ namespace Selenium
                 }
                 else
                 {
-                    MessageBox.Show("Не обрана швидкість прокрутки");
+                    MessageBox.Show("Не обрана швидкість пошуку");
                     return;
                 }
             }
@@ -60,9 +62,10 @@ namespace Selenium
                 await Task.Delay(3500);
                 IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
                 var buttonNextPage = driver.FindElement(By.CssSelector("#ppdPk-Ej1Yeb-LgbsSe-tJiF1e"));
-                string list = null;
-                int counterOfMarks = 0;
-
+                Dictionary<string, List<string>> list = new Dictionary<string, List<string>>();
+                int counterOfResults = 0;
+                string path = @"D:\\Programming_study\\Selenium\\Result\\list.xls";
+                File.Delete(path);
                 while (true)
                 {
                     await Task.Delay(2000);
@@ -94,29 +97,59 @@ namespace Selenium
                     {
                         try
                         {
-                            var oneRow = item.FindElement(By.XPath(".//div[@class='qBF1Pd fontHeadlineSmall']"));
-                            if (oneRow != null)
+                            var nameOneRow = item.FindElement(By.XPath(".//div[@class='qBF1Pd fontHeadlineSmall']"));
+                            if (nameOneRow != null)
                             {
-                                list += oneRow.Text + " | ";
-                                counterOfMarks++;
+                                var someContent = item.FindElements(By.XPath(".//*[@class='W4Efsd' and @jsinstance]"));
+                                List<string> contentOneRow = new List<string>();
+                                if (someContent != null)
+                                {
+                                    foreach (var item2 in someContent)
+                                        if (item2.Text != null)
+                                        {
+                                            contentOneRow.Add(item2.Text.ToString());
+                                        }
+                                }
+                                list[nameOneRow.Text] = contentOneRow;
+                                //list.Add(nameOneRow.Text, contentOneRow);
+                                counterOfResults++;
                             }
                         }
                         catch (System.Exception)
                         {
-
                             continue;
                         }
                     }
-
+                    
                     // output data from ListOfOnePage
                     try
                     {
-                        list += "\n";
-                        buttonNextPage.Click();
+                        using (StreamWriter stream = new StreamWriter(path, true))
+                        {
+                            foreach (var item in list)
+                            {
+                                stream.Write(item.Key);
+                                foreach (var item2 in item.Value)
+                                {
+                                    stream.Write(item2);
+                                }
+                                stream.WriteLine();
+                            }
+                        }
+                        /* foreach (var item in list)
+                         {
+                             System.IO.File.AppendAllText("D:\\Programming_study\\Selenium\\Result\\list.xls", item.Key + item.Value + "\n"); //Либо найти как открывать файл изменять, сохранять и закрывать либо попробовать dictionary
+                         }*/
+
+                        driver.Quit();
+                        break;
+                        //buttonNextPage.Click();
                     }
                     catch
                     {
-                        MessageBox.Show($"Программа завершила работу.\n {list} \n Всего найдено результатов: {counterOfMarks}");
+                        MessageBox.Show($"Программа завершила работу.\n {list} \n Всего найдено результатов: {counterOfResults}");
+                        //MessageBox.Show($"Программа завершила работу.\n {list} \n Всего найдено результатов: {counterOfMarks}");
+
                         driver.Quit();
                         break;
                     }
