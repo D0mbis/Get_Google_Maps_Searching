c48afb5fc4f13c106c4f27a55154c5f67d2c521b
Dombis <dombis.gg@gmail.com>
2022-06-21 18:39:00 +0300

    Include EPPlus library, chenged MainWindow, chenged all classes

---
+++ Methods/CheckUserInput.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+
+namespace Selenium
+{
+    internal class CheckUserInput : IDisposable
+    {
+        public int ScrollingDelay { get; private set; }
+        public bool Available { get; private set; }
+        public CheckUserInput(string text, bool? isChecked1, bool? isChecked2, bool? isChecked3)
+        {
+            Available = CheckingUserInput(text) &&
+            CheckingRadiobutton(isChecked1, isChecked2, isChecked3);
+        }
+
+        bool CheckingUserInput(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("You need input link to search.");
+                return false;
+            }
+            else if (!text.Contains("www.google"))
+            {
+                MessageBox.Show("Link must contain 'www.google.com/maps/' \n Try again.");
+                return false;
+            }
+            else if (text.Contains("www.google"))
+                return true;
+            else return false;
+        }
+
+        bool CheckingRadiobutton(bool? isChecked1, bool? isChecked2, bool? isChecked3)
+        {
+            int value = 2000, value1 = 1500, value2 = 1000;
+            {
+                if (isChecked1 == true)
+                {
+                    ScrollingDelay = value;
+                    return true;
+                }
+
+                else if (isChecked2 == true)
+                {
+                    ScrollingDelay = value1;
+                    return true;
+                }
+                else if (isChecked3 == true)
+                {
+                    ScrollingDelay = value2;
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("You need set speed searching.");
+                    return false;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- Methods/ExcelMethods.cs
+++ Methods/ExcelMethods.cs
@@ -1,126 +1,131 @@
 ﻿using Microsoft.Win32;
+using OfficeOpenXml;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
-using Excel = Microsoft.Office.Interop.Excel;
+
+//using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Selenium
 {
     internal class ExcelMethods : IDisposable
     {
-        private Excel.Application _excel = null;
-        private Excel.Workbook _workbook = null;
-        private Excel.Worksheet _sheet = null;
+
         private string path;
 
         public ExcelMethods()
         {
-            _excel = new Excel.Application();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             using (StreamReader stream = new StreamReader("path.txt")) { path = stream.ReadToEnd(); }
+
+        }
+        public string GetCorrectlyPath()
+        {
+            while (true)
+            {
+                if (File.Exists(path))
+                {
+                    try
+                    {
+                        var result = MessageBox.Show($"File: {path} will be overwrite. \n Are you sure?", "Worning", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            File.Delete(path);
+                            return path;
+                        }
+                        else
+                        {
+                            SaveAs();
+                            continue;
+                        }
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    var result = MessageBox.Show($"Do you want save file results in: {path}?", "Worning", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        return path;
+                    }
+                    else
+                    {
+                        SaveAs();
+                        continue;
+                    }
+                }
+            }
         }
 
         public string SaveAs()
         {
-            try
+            while (true)
             {
-                SaveFileDialog saveDialog = new SaveFileDialog();
-                saveDialog.Filter = "Книга Excel 97-2003 (*.xlsx) | *.xlsx";
-                if (saveDialog.ShowDialog() == true)
+                try
                 {
-                    path = saveDialog.FileName.ToString();
+                    SaveFileDialog saveDialog = new SaveFileDialog
+                    {
+                        Filter = "Книга Excel 97-2003 (*.xlsx) | *.xlsx"
+                    };
+                    if (saveDialog.ShowDialog() == true)
+                    {
+                        path = saveDialog.FileName.ToString();
+                    }
+                    return path;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        public string SaveExcelFileNew(Dictionary<string, List<string>> dictionaryOfResults, string path)
+        {
+
+            using (ExcelPackage package = new ExcelPackage(path))
+            {
+                var _worksheet = package.Workbook.Worksheets.Add("Results");
+                int rowNumber = 1, columnNumber;
+                foreach (var item in dictionaryOfResults)
+                {
+                    columnNumber = 1;
+                    _worksheet.Cells[rowNumber, columnNumber].LoadFromText(item.Key);
+                    foreach (var item2 in item.Value)
+                    {
+                        columnNumber++;
+                        try
+                        {
+                            _worksheet.Cells[rowNumber, columnNumber].LoadFromText(item2);
+                        }
+                        catch { }
+                    }
+                    rowNumber++;
+                }
+
+                try
+                {
+                    _worksheet.Cells.Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                    _worksheet.Cells.AutoFitColumns();
+                    package.Save();
+                    MessageBox.Show($"Results were saved: {dictionaryOfResults.Count}", "Successfully completed", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch 
+                {
+                    MessageBox.Show("Save Excel file does`t complete successfully.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
                 return path;
             }
-            catch
-            {
-                return null;
-            }
         }
 
-        internal void Open()
-        {
-            if (File.Exists(path))
-            {
-                _workbook = _excel.Workbooks.Open(path);
-                _workbook.Worksheets[1].Cells.ClearContents();
-            }
-            else
-            {
-                try
-                {
-                    _workbook = _excel.Workbooks.Add();
-                    _sheet = _workbook.Sheets.Add();
-                    _sheet.Name = "Results List";
-                }
-                catch
-                {
-                    Dispose();
-                    MessageBox.Show("Error! \n Excel does not start, the version may be incompatible");
-                }
-            }
-        }
-
-        internal void ToExcel(int row, int column, object data)
-        {
-            _workbook.ActiveSheet.Cells[row, column] = data;
-        }
-
-        public string Save()
-        {
-            try
-            {
-                if (File.Exists(path))
-                {
-                    _workbook.SaveAs(path);
-                    using (StreamWriter stream = new StreamWriter("path.txt")) { stream.Write(path); }
-                    MessageBox.Show("Results was saved successfully!");
-                }
-                else
-                {
-                    MessageBox.Show("Save error! \n You need to correctly set the path to save the file. Try again");
-                    SaveAs();
-                    _workbook.SaveAs(path);
-                    using (StreamWriter stream = new StreamWriter("path.txt")) { stream.Write(path); }
-                    MessageBox.Show("Results was saved successfully!");
-
-                }
-            }
-            catch
-            {
-                //MessageBox.Show("You need to set the path to the file with the results!");
-                do
-                {
-                    MessageBox.Show("Save error! \n You need to correctly set the path to save the file. Try again");
-                    SaveAs();
-                    try
-                    {
-                        _workbook.SaveAs(path);
-                        break;
-                    }
-                    catch
-                    {
-                    }
-                }
-                while (true);
-                using (StreamWriter stream = new StreamWriter("path.txt")) { stream.Write(path); }
-                MessageBox.Show("Results was saved successfully!");
-            }
-            while (true)
-                try
-                {
-                    _workbook.Close();
-                    _excel.Quit();
-                }
-                catch
-                {
-                    _excel.Quit();
-                    break;
-                }
-            return path;
-        }
         public void Dispose()
         {
-           
+
         }
     }
 }
